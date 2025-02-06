49344a44473f37ed1b8fc78b90fce92a00ce614f
allysonfellipo <allysonfelippo35@gmail.com>
2025-02-05 22:17:34 -0300

    Componentes novos

---
+++ revisaozona/src/components/Body.jsx
@@ -0,0 +1,13 @@
+import './Body.css';
+import Demo1 from './Demo1';
+
+function Body() {
+  return (
+    <div className="container-body">
+        <h1>Página importante</h1>
+        <Demo1 />
+    </div>
+  )
+}
+
+export default Body
